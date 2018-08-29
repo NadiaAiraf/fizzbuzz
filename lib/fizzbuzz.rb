761cfe3ac6140c981574554c7246d8d573c2169b
Alex <akharoukpp@gmail.com>
2018-08-29 15:13:34 +0100

    add more tests

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,11 @@
 def fizzbuzz(number)
-  if number % 3 == 0
-  'fizz'
+  if number % 3 == 0 && number != 15
+    'fizz'
+  elsif number == 15
+    'FizzBuzz'
+  elsif number % 5 == 0
+    'buzz'
   else
-  'buzz'
+    number
   end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -9,12 +9,35 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
 
-  it 'returns "fizz" when passed number divisible by 3 of maximum 20' do
+  it 'returns "fizz" when passed number divisible by 3 and not divisible by 5 - maximum of 20' do
     expect(fizzbuzz(3)).to eq 'fizz'
     expect(fizzbuzz(6)).to eq 'fizz'
     expect(fizzbuzz(9)).to eq 'fizz'
     expect(fizzbuzz(12)).to eq 'fizz'
-    expect(fizzbuzz(15)).to eq 'fizz'
     expect(fizzbuzz(18)).to eq 'fizz'
+  end
+
+  it 'returns "buzz" when passed number divisible by 5 and not divisible by 3 - maximum of 20' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+    expect(fizzbuzz(10)).to eq 'buzz'
+    expect(fizzbuzz(20)).to eq 'buzz'
+  end
+
+  it 'returns "FizzBuzz" when passed number divisible by 3 and 5 - max of 20' do
+    expect(fizzbuzz(15)).to eq 'FizzBuzz'
+  end
+
+  it 'returns "number" when passed number NOT divisible by 3 or 5 or both - max of 20' do
+    expect(fizzbuzz(1)).to eq 1
+    expect(fizzbuzz(2)).to eq 2
+    expect(fizzbuzz(4)).to eq 4
+    expect(fizzbuzz(7)).to eq 7
+    expect(fizzbuzz(8)).to eq 8
+    expect(fizzbuzz(11)).to eq 11
+    expect(fizzbuzz(13)).to eq 13
+    expect(fizzbuzz(14)).to eq 14
+    expect(fizzbuzz(16)).to eq 16
+    expect(fizzbuzz(17)).to eq 17
+    expect(fizzbuzz(19)).to eq 19
   end
 end
