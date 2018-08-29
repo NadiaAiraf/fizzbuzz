3bef02d0a5fc3936bce993883e7076c8cae7fc9f
Alex <akharoukpp@gmail.com>
2018-08-29 14:57:54 +0100

    third test - divisible by 3

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,17 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
+  it 'returns "fizz" when passed number divisible by 3 of maximum 20' do
+    expect(fizzbuzz(3)).to eq 'fizz'
+    expect(fizzbuzz(6)).to eq 'fizz'
+    expect(fizzbuzz(9)).to eq 'fizz'
+    expect(fizzbuzz(12)).to eq 'fizz'
+    expect(fizzbuzz(15)).to eq 'fizz'
+    expect(fizzbuzz(18)).to eq 'fizz'
+  end
 end
